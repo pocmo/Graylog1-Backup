a92520ad3ad38e006bd84b407172be0ffc0c5fe0
Lennart Koopmann <lennart@linux.jimdo.office>
2009-10-06 16:51:44 +0200

    Refactored whole dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,32 +1,50 @@
 class DashboardController < ApplicationController
 
   def index
-    if Setting.last.blank?
-      @timespan = 10
-      @number_of_allowed_messages = 100
-      @fontsize = 50
-    else
-      Setting.last.dashboard_timespan.blank? ? @timespan = 10 : @timespan = Setting.last.dashboard_timespan.to_i
-      Setting.last.dashboard_messages.blank? ? @number_of_allowed_messages = 100 : @number_of_allowed_messages = Setting.last.dashboard_messages.to_i
-      Setting.last.dashboard_font_size.blank? ? @fontsize = 50 : @fontsize = Setting.last.dashboard_font_size.to_i
-    end
-    blacklist_conditions = build_conditions_from_blacklist
-    timespan_condition = [ "ReceivedAt > ?", @timespan.minutes.ago]
-    conditions = Logentry.merge_conditions blacklist_conditions, timespan_condition
-    @new_messages = Logentry.count :conditions => conditions
-    
-    @alert = alert? @number_of_allowed_messages, @new_messages
+    @dashboard_settings = get_dashboard_settings
+    @new_messages = get_new_messages @dashboard_settings["timespan"]
+    @alert = alert? @dashboard_settings["number_of_allowed_messages"], @new_messages
   end
   
   def nagioscheck
-    render :text => "okay" if !alert? @number_of_allowed_messages, @new_messages
+    @dashboard_settings = get_dashboard_settings
+    @new_messages = get_new_messages @dashboard_settings["timespan"]
+    
+    if !alert? @dashboard_settings["number_of_allowed_messages"], @new_messages
+      render :text => "okay"
+      return
+    end
+    
     render :text => "alert"
   end
   
   private
+  def get_new_messages timespan
+    blacklist_conditions = build_conditions_from_blacklist
+    timespan_condition = [ "ReceivedAt > ?", timespan.minutes.ago]
+    conditions = Logentry.merge_conditions blacklist_conditions, timespan_condition
+    return Logentry.count :conditions => conditions
+  end
+  
+  def get_dashboard_settings
+    if Setting.last.blank?
+      timespan = 10
+      number_of_allowed_messages = 100
+      fontsize = 50
+    else
+      Setting.last.dashboard_timespan.blank? ? timespan = 10 : timespan = Setting.last.dashboard_timespan.to_i
+      Setting.last.dashboard_messages.blank? ? number_of_allowed_messages = 100 : number_of_allowed_messages = Setting.last.dashboard_messages.to_i
+      Setting.last.dashboard_font_size.blank? ? fontsize = 50 : fontsize = Setting.last.dashboard_font_size.to_i
+    end
+    
+    return { "number_of_allowed_messages" => number_of_allowed_messages,
+             "timespan" => timespan,
+             "fontsize" => fontsize }
+  end
+  
   def alert? number_of_allowed_messages, new_messages
-    true if new_messages > number_of_allowed_messages
-    false
+    return true if new_messages > number_of_allowed_messages
+    return false
   end
 
 end
