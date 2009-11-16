f1954e78df1059734ed9715585a4fab815ffec88
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-11-16 19:24:10 +0100

    api: added ping, and offset to getmessages call

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,21 @@
 class ApiController < ApplicationController
   
+  def ping
+    # just to check if there's a graylog installation answering
+    render :text => 'GrayLog/Pong'
+  end
+  
   def getcategories
     categories = Category.find :all
     render :text => categories.to_json
   end
   
   def getmessages
-    limit = params[:limit]
+    limit = params[:limit].to_i
     limit = 50 if limit.blank? or limit > 250
+
+    offset = params[:offset].to_i
+    offset = 0 if offset.blank?
 
     if params[:start_from_id].blank?
       start_condition = Array.new
@@ -24,7 +32,7 @@ class ApiController < ApplicationController
     # Merge all conditions
     conditions = Logentry.merge_conditions start_condition, category_condition
 
-    logmessages = Logentry.find :all, :conditions => conditions, :limit => limit, :order => "ID DESC"
+    logmessages = Logentry.find :all, :conditions => conditions, :limit => limit, :offset => offset, :order => "ID DESC"
     render :text => logmessages.to_json
   end
   
