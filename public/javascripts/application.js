ad50eac53cc679bd55a2837e8ffc5da06b0c5ec8
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-03-01 21:09:17 +0100

    broken indentation!?

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,99 +1,96 @@
 (function($){
 	
 GL = new function () {
-	var self = this;
+    var self = this;
 
-	var $tbl,
-	    reallyMsg = 'Are you sure?';
+    var $tbl,
+        reallyMsg = 'Are you sure?';
 
-	this.init = function() {
-		$tbl = $('#gl-table');
-		$(document.body).actionController(self);
-	};
+    this.init = function() {
+        $tbl = $('#gl-table');
+        $(document.body).actionController(self);
+    };
 
-	this.checkAllClick = function() {
-		$('input:checkbox',$tbl).attr('checked', this.checked ? 'checked' : '');	
-	};
+    this.checkAllClick = function() {
+        $('input:checkbox',$tbl).attr('checked', this.checked ? 'checked' : '');
+    };
 
-	this.deleteClick = function() {
-		var items = serializeCheckboxes();
-		if (items) {
-			$.post("/logentries/destroy/", {
-				authenticity_token: self.token,
-				items: items
-					
-			},function(){
-				location.reload();
-			});	
-		};
+    this.deleteClick = function() {
+        var items = serializeCheckboxes();
+        if (items) {
+            $.post("/logentries/destroy/", {
+                authenticity_token: self.token,
+                items: items
 
-		return false;
-	};
+            }, function(){
+                location.reload();
+            });
+        };
 
-	this.addFavoriteClick = function() {
-		var url = this.checked ? "/categories/favorite/" : '/categories/unfavorite/';		
-		url+=this.value;
-		$.get(url);
-	};
-	
-	this.markAsValidClick = function() {
-		var url = '/validmessages/create/';		
-		var items = serializeCheckboxes();
-		if (items) {
-			$.post(url, {
-				authenticity_token: self.token,
-				items: items
-			},function(){
-        location.reload();
-			});	
-		};
+        return false;
+    };
 
-		return false;
-	};
+    this.addFavoriteClick = function() {
+        var url = this.checked ? "/categories/favorite/" : '/categories/unfavorite/';
+        url+=this.value;
+        $.get(url);
+    };
 
-	this.markAsInvalidClick = function() {
-		var url = "/validmessages/destroy/";		
-		var items = serializeCheckboxes();
-		if (items) {
-			$.post(url, {
-				authenticity_token: self.token,
-				items: items
-			},function(){
-        location.reload();
-			});	
-		};
+    this.markAsValidClick = function() {
+        var url = '/validmessages/create/';
+        var items = serializeCheckboxes();
+        if (items) {
+            $.post(url, {
+                    authenticity_token: self.token,
+                    items: items
+            },function(){
+                location.reload();
+            });
+        };
 
-		return false;
-	};
+        return false;
+    };
 
-  this.showFullMessageClick = function(e, value) {
-    $('#gl-message-' + value).remove();
-    $('#gl-message-full-' + value).fadeIn();
-    return false;
-  }
+    this.markAsInvalidClick = function() {
+            var url = "/validmessages/destroy/";
+            var items = serializeCheckboxes();
+            if (items) {
+                $.post(url, {
+                        authenticity_token: self.token,
+                        items: items
+                },function(){
+                    location.reload();
+                });
+            };
 
-	function serializeCheckboxes() {
-		var data = $('input:checkbox:checked').map(function(){
-			return this.value;
-		});
-		data = $.makeArray(data);
-		data[0] == 'checkAll' && data.pop();
-		
+        return false;
+    };
 
-		var ret = data.length>1 ? confirm(reallyMsg) : true;
+    this.showFullMessageClick = function(e, value) {
+        $('#gl-message-' + value).remove();
+        $('#gl-message-full-' + value).fadeIn();
+        return false;
+    }
 
-		var items = '';
-		$.each(data, function(i, val){
-			items = items.length ? items+ ',' : '';
-			items+=val;
-		});
-		return ret ? items : false;		
+    function serializeCheckboxes() {
+        var data = $('input:checkbox:checked').map(function(){
+                return this.value;
+        });
+        data = $.makeArray(data);
+        data[0] == 'checkAll' && data.pop();
 
-	};
+
+        var ret = data.length>1 ? confirm(reallyMsg) : true;
+
+        var items = '';
+        $.each(data, function(i, val){
+            items = items.length ? items+ ',' : '';
+            items+=val;
+        });
+        return ret ? items : false;
+    };
 
 };
-
-
 
 $(function(){
 	GL.init();	
